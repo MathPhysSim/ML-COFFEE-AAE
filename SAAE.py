8657514765bd9be7626efdf2ab0c63018478d80e
Simon Hirlander <simon.hirlaender@cern.ch>
2019-07-04 14:28:59 +0200

    add init

--- SAAE.py
+++ SAAE.py
@@ -195,3 +195,21 @@ if __name__ == '__main__':
     L = helpers.approximateLogLiklihood(generated, x_test)
     print("Log Likelihood")
     print(L)
+
+def run_script():
+    # Load MNIST dataset
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    idx_unlabel = np.random.randint(0, x_train.shape[0], 20000)
+    y_train[idx_unlabel] = 10
+    x_train = x_train.astype(np.float32) / 255.
+    x_test = x_test.astype(np.float32) / 255.
+    ann = SSAAE()
+    vecs, b = ann.generateRandomVectors(1000 * list(range(10)))
+    plt.scatter(vecs[:, 0], vecs[:, 1])
+    ann.train(x_train, y_train, x_test, y_test, epochs=10000)
+    vecs, b = ann.generateRandomVectors(1000 * list(range(10)))
+    generated = ann.decoder.predict(vecs)
+    print(generated.shape)
+    L = helpers.approximateLogLiklihood(generated, x_test)
+    print("Log Likelihood")
+    print(L)
