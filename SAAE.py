f6941e2c6620d45a8453ae03d5f51d3c920701e7
Simon Hirlander <simon.hirlaender@cern.ch>
2019-07-04 14:31:58 +0200

    add init

--- SAAE.py
+++ SAAE.py
@@ -177,6 +177,24 @@ class SSAAE():
             vectors.append(vec)
         return (np.array(vectors).reshape(-1, 2), labels)
 
+def run_script():
+    # Load MNIST dataset
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    idx_unlabel = np.random.randint(0, x_train.shape[0], 20000)
+    y_train[idx_unlabel] = 10
+    x_train = x_train.astype(np.float32) / 255.
+    x_test = x_test.astype(np.float32) / 255.
+    ann = SSAAE()
+    vecs, b = ann.generateRandomVectors(1000 * list(range(10)))
+    plt.scatter(vecs[:, 0], vecs[:, 1])
+    ann.train(x_train, y_train, x_test, y_test, epochs=10000)
+    vecs, b = ann.generateRandomVectors(1000 * list(range(10)))
+    generated = ann.decoder.predict(vecs)
+    print(generated.shape)
+    L = helpers.approximateLogLiklihood(generated, x_test)
+    print("Log Likelihood")
+    print(L)
+
 
 if __name__ == '__main__':
     # Load MNIST dataset
@@ -196,20 +214,3 @@ if __name__ == '__main__':
     print("Log Likelihood")
     print(L)
 
-def run_script():
-    # Load MNIST dataset
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    idx_unlabel = np.random.randint(0, x_train.shape[0], 20000)
-    y_train[idx_unlabel] = 10
-    x_train = x_train.astype(np.float32) / 255.
-    x_test = x_test.astype(np.float32) / 255.
-    ann = SSAAE()
-    vecs, b = ann.generateRandomVectors(1000 * list(range(10)))
-    plt.scatter(vecs[:, 0], vecs[:, 1])
-    ann.train(x_train, y_train, x_test, y_test, epochs=10000)
-    vecs, b = ann.generateRandomVectors(1000 * list(range(10)))
-    generated = ann.decoder.predict(vecs)
-    print(generated.shape)
-    L = helpers.approximateLogLiklihood(generated, x_test)
-    print("Log Likelihood")
-    print(L)
